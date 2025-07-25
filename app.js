4ff721d25a60840d468a6729bca1e47037d72f0e
HasanLabib <hasanlabib3@gmail.com>
2025-07-25 10:55:34 +0600

    6th_Commit_Added_Footer Section and made it responsive and also improved some ui design andd hover effect on all button and log

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 function addcart() {
   alert("Flower Added to Cart");
 }
+
+function getUserEmail() {
+  let userEmail = document.getElementById("email");
+  const emailAddress = userEmail.value;
+  console.log(emailAddress);
+  alert(
+    "Your Email: " +
+      '"' +
+      emailAddress +
+      '"' +
+      " is added to our database. Thanks We will inform you about aur promotion preodically"
+  );
+}
